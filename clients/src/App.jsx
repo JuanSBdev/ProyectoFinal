bc51430732d4c043feddae84a1bd71d29a69b6f2
Arivera <alvaroriverafernandez@gmail.com>
2023-08-25 16:16:05 -0400

    CAMBIOS EN CARDS

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -1,21 +1,21 @@
-import Start from "./pages/Landing/Start";
-import Form from "./pages/CreateProduct/Form";
-import Home from "./pages/Home/Home";
-import NotFound from "./pages/NotFound/NotFound";
-import Detail from "./pages/Detail/Detail";
+import Start from './pages/Landing/Start'
+import Form from './pages/CreateProduct/Form'
+import Home from './pages/Home/Home'
+import NotFound from './pages/NotFound/NotFound'
+import Detail from './pages/Detail/Detail'
 import BuyView from './pages/Buy/BuyView'
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
     <div>
       <Routes>
-          <Route exact path="/" Component={Start}/>
-          <Route exact path="/home" Component={Home}/>
-          <Route exact path="/form" Component={Form}/>
-          <Route exact path="/detail:id" Component={Detail}/>
-          <Route exact path="/products" Component={BuyView}/>
-          <Route path="*" Component={NotFound}/>
+        <Route exact path="/" Component={Start} />
+        <Route exact path="/home" Component={Home} />
+        <Route exact path="/form" Component={Form} />
+        <Route exact path="/detail:id" Component={Detail} />
+        <Route exact path="/products" Component={BuyView} />
+        <Route path="*" Component={NotFound} />
       </Routes>
     </div>
   )
--- clients/src/components/Card/Card.jsx
+++ clients/src/components/Card/Card.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import PropTypes from 'prop-types'
 import Styles from './Card.module.css'
-import {NavLink} from 'react-router-dom'
 import {
   AiOutlineHeart,
   AiFillPlusCircle,
