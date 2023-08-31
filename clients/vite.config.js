5fa7c29ed183d0f62566dbb67da01e7c853e3cad
Juan SB <juansb.dev@gmail.com>
2023-08-31 19:35:53 -0300

    deployar2

--- clients/vite.config.js
+++ clients/vite.config.js
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     port: 3002,
-    mimeTypes: {
-      'application/javascript': ['js'],
-    },
+   
   },
   build: {
     rollupOptions: {
